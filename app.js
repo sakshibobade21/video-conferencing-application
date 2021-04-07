018bd177ef7fa4017fc936926cb2158b4268c747
Sakshi Bobade <bobadesakshi21@gmail.com>
2021-04-07 08:12:42 +0530

    Added the ability to view our own video

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
-app.use(express.static('public'))
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 const roomRoute = require('./routes/room')
 
--- public/script.js
+++ public/script.js
@@ -1,1 +1,28 @@
-console.log('Script file')
+const videoGrid = document.getElementById('video-grid')
+
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+let myVideoStream
+
+// Get stream if user gives the permission
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  myVideoStream = stream
+  addVideoStream(myVideo, myVideoStream)
+})
+
+const addVideoStream = (video, stream) => {
+  // Display the video so user can see himself
+  video.srcObject = stream
+
+  // When metadata like duration, dimensions etc for the specified audio/video has been loaded.
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+
+  // Add this video to the video grid
+  videoGrid.append(video)
+}
