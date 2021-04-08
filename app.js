ec97af24b211f9d80adbba00091c440dc2b50802
Sakshi Bobade <bobadesakshi21@gmail.com>
2021-04-08 11:20:09 +0530

    Peer Server set up

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-
 const server = app.listen(3000, () => console.log('Server is up and running'))
 
+const { ExpressPeerServer } = require('peer')
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+})
+
 const io = require('./socket').init(server)
+
+app.user('/peerjs', peerServer)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
